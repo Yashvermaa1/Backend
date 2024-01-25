fca09a193c800f80e105b8e246c63244b8faa2df
Yashvermaa1 <154309809+Yashvermaa1@users.noreply.github.com>
2024-01-25 20:37:46 +0530

    api

--- src/index.js
+++ src/index.js
@@ -9,3 +9,16 @@ dotenv.config({
     path: './env'
 });
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log( `Server is running at port : ${process.env.PORT}`);
+    }
+    app.on("error", (error) => {
+        console.log("ERROR: ", err)
+        throw error
+    })
+    )
+})
+.catch((err) => {
+    console.log("MANGO DB connection failed !!!", err);
+})
