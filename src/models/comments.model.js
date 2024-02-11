8e40c45b33b017676af930103a3385f1b64a86e2
Yashvermaa1 <154309809+Yashvermaa1@users.noreply.github.com>
2024-02-11 13:33:25 +0530

    ok

--- src/models/comments.model.js
+++ src/models/comments.model.js
@@ -1,7 +1,24 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, {Schema, Types} from "mongoose";
+import { Video } from "./video.model";
+import { User } from "./user.model";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const commentsSchema = new Schema(
     {
-
+        content:{
+            type:"String",
+            required:true,
+        },
+        videos:{
+            type:Schema.Types.ObjectId,
+            ref:Video
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:User
+        }
     },{timestamps:true})
+
+    commentsSchema.plugin(mongooseAggregatePaginate)
+
 
 export const Comments = mongoose.model("Comments", commentsSchema)
--- src/models/likes.model.js
+++ src/models/likes.model.js
@@ -1,7 +1,16 @@
 import mongoose, {Schema} from "mongoose";
+import { User } from "./user.model";
+import { Video } from "./video.model";
 const likesSchema = new Schema(
     {
-
+        likedBy:{
+            type:Schema.Types.ObjectId,
+            ref:User
+        },
+        videos:{
+            type:Schema.Types.ObjectId,
+            ref:Video
+        }
     },{timestamps:true})
 
 export const Likes = mongoose.model("Likes", likesSchema)
