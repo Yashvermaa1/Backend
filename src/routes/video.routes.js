a58f14b67b0a5d256300af37ac60122ae4f17bca
Yashvermaa1 <154309809+Yashvermaa1@users.noreply.github.com>
2024-02-11 22:30:13 +0530

    ok

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,5 +1,25 @@
 import { Router } from "express";
-
+import {gettAllVideos, publishAVideo, getVideoById, updateVideo, deleteVideo, togglePublishStatus} from '../controllers/video.controller.js'
+import {upload} from '../middlewares/multer.middlewares.js'
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
+
+router.route("/").get(gettAllVideos).post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount: 1
+        },
+        {
+            name : "coverImage",
+            maxCount: 1
+        }
+    ]),
+    publishAVideo
+)
+router.route("/search").get(getVideoById)
+router.route("/update-video").patch(verifyJWT, upload.single("thumbnail") , updateVideo)
+router.route("/delete-video").patch(verifyJWT, deleteVideo)
+router.route("/toggle/publish/:videoId").patch( togglePublishStatus);
 export default router
